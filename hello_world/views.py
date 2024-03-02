641564132f7924cbdc7211001197ce8a33590b70
martinamicaela <martinamicaela924@gmail.com>
2024-03-02 22:55:08 +0000

    fix indentation method.post func

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def index(request):
      if request.method == "POST":
         return HttpResponse("You must have POSTed something")
-    else:
+     else:
         return HttpResponse(request.method)
